a60f5d4f4e7bb338e81173666e94b62f70261775
Ammar Sherif <ammarsherif90@gmail.com>
2022-10-16 21:29:09 +0200

    Added the gaussian MAE

---
+++ gaussian-mae/models_mae.py
@@ -0,0 +1,310 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+
+from functools import partial
+
+import torch
+import torch.nn as nn
+
+import numpy as np
+
+from timm.models.vision_transformer import PatchEmbed, Block
+
+from util.pos_embed import get_2d_sincos_pos_embed
+from util.gaussian_sampler import mixture_gaussians_1D, generate_binary_mask_1D, sample_indices_1D
+
+class MaskedAutoencoderViT(nn.Module):
+    """ Masked Autoencoder with VisionTransformer backbone
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., num_gaussians= 5, gaussian_size = 7, 
+                 norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        """
+        - img_size: the width and height of the image [ex: 224 for 224x224]
+        - patch_size: the dimension in pixels of a single patch [ex: 16 for 16x16]
+        - in_chans: the number of input channels
+        - embed_dim: the embedding dimension for the encoder
+        - depth: the encoder depth
+        - num_heads: the encoder number of heads
+        - decoder_embed_dim: the embedding dimension for the decoder
+        - decoder_depth: the decoder depth
+        - decoder_num_heads: the decoder number of heads
+        - mlp_ratio: the mlp ratio
+        - num_gaussians: the number of generated gaussians for each image
+        - gaussian_size: the diameter of a single gaussian in terms of patches
+        """
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+        
+        self.__num_gaussians = num_gaussians
+        self.__gaussian_size = gaussian_size
+        
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * in_chans, bias=True) # decoder to patch
+        # --------------------------------------------------------------------------
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def patchify(self, imgs):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        """
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
+        return x
+
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1]**.5)
+        assert h * w == x.shape[1]
+        
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+
+    def gaussian_masking(self, x, num_samples):
+        """
+        Perform masking according to the random gaussian
+        x: [N, L, D], sequence
+        """
+        N, L, D = x.shape  # batch, length, dim
+        h = w = int(L**.5)
+        assert h * w == L
+        
+        # Generate window sizes; all are essentially of the same size
+        window_sizes = torch.ones(self.__num_gaussians, dtype=torch.uint8)*self.__gaussian_size
+
+        p_mask, means_mask = mixture_gaussians_1D(N,self.__num_gaussians, 
+                                                  window_sizes, h, w)
+        
+        means_mask = means_mask.to(x.device)
+        p_mask = p_mask.to(x.device)
+        
+        # --------------------------------------------------------------------------
+        # Now we  have the  probability  distribution from which  we will sample the
+        # reconstructed [p_mask], and the means to be used for reconstruction.  Yet,
+        # we shuffle the data further, so the means are not always in order
+        # --------------------------------------------------------------------------
+        # means_idx = means_mask.nonzero()[:,1].reshape(N,-1)
+
+        # Shuffle the means, 
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+        ids_shuffle = torch.argsort(noise, dim=1)
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+
+        means_mask = torch.gather(means_mask, dim=1, index=ids_shuffle)
+        mask_sort_ids = torch.argsort(1-means_mask, dim=1)
+        mask_ids_restore = torch.argsort(mask_sort_ids, dim=1)
+        # --------------------------------------------------------------------------
+        # Now, these indices represent the shuffled data
+        means_idx = mask_sort_ids[:,:self.__num_gaussians]
+        x = torch.gather(x, dim=1, index=ids_shuffle.unsqueeze(-1).repeat(1, 1, D))
+        x_masked = torch.gather(x, dim=1, index=means_idx.unsqueeze(-1).repeat(1,
+                                                                             1, D))
+        
+        # --------------------------------------------------------------------------
+        # Now, sample the patches for reconstruction
+        # --------------------------------------------------------------------------
+        sample_idx = sample_indices_1D(p_mask, num_samples)
+        # 1 -> for patches to be reconstructed
+        mask = generate_binary_mask_1D(p_mask.shape, sample_idx)
+        # --------------------------------------------------------------------------
+        # now modify the values s.t 0 is [DOES NOT MATTER], 1 is remove
+        mask = mask.to(x.device)
+        # --------------------------------------------------------------------------
+        # The below is just to make sure we did not sample a single patch for both
+        # --------------------------------------------------------------------------
+        means_mask = torch.gather(means_mask, dim=1, index=ids_restore)
+        assert (mask*means_mask).sum() == 0
+
+        return x_masked, mask_ids_restore, ids_restore, mask                                                  
+
+    def forward_encoder(self, x, reconstruction_per_gaussian):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        num_samples = np.clip(int(self.__num_gaussians*reconstruction_per_gaussian),
+                              1,x.shape[1]-self.__num_gaussians)
+
+        # --------------------------------------------------------------------------
+        # masking: get the patches used for reconstruction, two indices for unshuff- 
+        # le, and the mask for our target tokens
+        x, mask_ids_restore,ids_restore,mask = self.gaussian_masking(x, num_samples)
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask_ids_restore, ids_restore, mask
+
+    def forward_decoder(self, x, mask_ids_restore, ids_restore):
+        
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        # Back to the original shuffled
+        x_ = torch.gather(x_, dim=1, index=mask_ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2])) 
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
+        """
+        imgs: [N, 3, H, W]
+        pred: [N, L, p*p*3]
+        mask: [N, L], 0 is keep, 1 is remove, 
+        """
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
+            mean = target.mean(dim=-1, keepdim=True)
+            var = target.var(dim=-1, keepdim=True)
+            target = (target - mean) / (var + 1.e-6)**.5
+
+        loss = (pred - target) ** 2
+        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        return loss
+
+    def forward(self, imgs, reconstruction_per_gaussian=3):
+        latent, mask_ids_restore, ids_restore, mask = self.forward_encoder(imgs,
+                                                    reconstruction_per_gaussian)
+        pred = self.forward_decoder(latent, mask_ids_restore, ids_restore)  # [N, L, p*p*3]
+        loss = self.forward_loss(imgs, pred, mask)
+        return loss, pred, mask
+
+
+def mae_vit_base_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_large_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_huge_patch14_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=14, embed_dim=1280, depth=32, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+# set recommended archs
+mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
---
+++ gaussian-mae/util/gaussian_sampler.py
@@ -0,0 +1,225 @@
+# ==============================================================================
+# This file includes helper methods to use the gaussian sampler
+# ==============================================================================
+# This source  code is licensed under the license found in the  LICENSE  file in 
+# the root directory of this source tree.
+# ------------------------------------------------------------------------------
+import torch
+
+def get_mean_dim(m_dim, win, dim_size):
+    """returns the mean point for a particular  dimension for our gaussian gene-
+    ration to make sure the distribution remains within the image"""
+    if win>dim_size:
+        m_dim = torch.ones(m_dim.shape)*dim_size//2
+    else:
+        min_m_dim = win//2
+        max_m_dim = dim_size-min_m_dim
+        m_dim = torch.clip(m_dim, min_m_dim, max_m_dim)
+    return m_dim
+
+def generate_gaussian_2D_1D(h, w, m_h, m_w, win, enable_out = True,
+                            mean_mask = True):
+    """generates a 2D gaussian distribution in a matrix and 
+    
+    - h: 2D grid height
+    - w: 2D grid width
+    - m_h: the mean points with respect to the height axis
+    - m_w: the mean points with respect to the width axis
+    - win: window size"""
+    N = m_h.shape[0]
+    # we scale the range of values as the number of points is  different to have
+    # an indentity covariance matrix
+    min_dim = min(h, w)
+    h_ratio = h/min_dim
+    w_ratio = w/min_dim
+
+
+    # to make sure we do not go out of the image while generating
+    if not enable_out:
+        m_h = get_mean_dim(m_h, win, h)
+        m_w = get_mean_dim(m_w, win, w)
+
+    # define the range of values accroding to the mean h and w
+    S_H = -2*m_h/h
+    E_H = 2-2*m_h/h
+    S_W = -2*m_w/w
+    E_W = 2-2*m_w/w
+
+
+    # ==========================================================================
+    # TODO: modify the code to generate N 2D gaussians
+    dist = torch.empty(size=(N, h*w))
+    means_mask = None
+    if mean_mask:
+        means_mask = torch.zeros(dist.shape)
+    i = -1
+    for s_h, e_h, s_w, e_w in zip(S_H, E_H, S_W, E_W):
+        i += 1
+        s_h, e_h, s_w, e_w = s_h.item(), e_h.item(), s_w.item(), e_w.item()
+        # now, generate the grid
+        x, y = torch.meshgrid(torch.linspace(s_h*h_ratio,e_h*h_ratio,h),
+                              torch.linspace(s_w*w_ratio,e_w*w_ratio,w),
+                              indexing='ij')
+        d = torch.sqrt(x*x+y*y)
+        # define the sigma according to the window size
+        # ----------------------------------------------------------------------
+        # we divide win/min_dim to be 1 when we sample from the whole  image, so
+        # sigma would span the whole window in such case. We further divide over
+        # 3, so~%100 of the distribution will be within the range of -1,1 (which
+        # is our window).
+        # ----------------------------------------------------------------------
+        sigma = win/min_dim/3
+        # now, generate the 2D gaussian
+        g = torch.exp(-( (d)**2 / ( 2.0 * sigma**2 ) ) )
+        if mean_mask:
+            g[m_h[i].item(),m_w[i].item()] = 0
+            mean_indx = m_h[i].item()*w+m_w[i].item()
+            means_mask[i,mean_indx] = 1
+        # return the normalized version of it
+        dist[i,:] = torch.reshape(g/g.sum(), (-1,))
+    return dist, means_mask
+
+def sample_indices_1D(p_mask, num_samples):
+    """The method samples patches from a 1D mask and return their indices"""
+    idx = p_mask.multinomial(num_samples=num_samples, replacement=False)
+    # return the indices
+    return idx
+
+def generate_binary_mask_1D(mask_shape, idx):
+    """The method generates a binary mask according to the samples
+    
+    - mask_shape: the shape of the generated mask
+    - idx: a tensor the includes the indices of the selected patches
+    """
+    b_mask = torch.zeros(mask_shape, dtype=torch.uint8)
+    b_mask[((torch.arange(b_mask.shape[0]).unsqueeze(1).repeat(1,idx.shape[1])), idx)] = 1
+    assert (b_mask.sum(dim=1) == idx.shape[1]).prod() == 1
+    return b_mask
+
+def mean_generation(window_size, N, n_gaussian, max_dims):
+    """the method randomly picks mean points for our gaussian generation
+
+    - window_size: the size of the generated gaussians
+    - N: number of patch grids
+    - n_gaussian: the number of gaussians per a single image
+    - max_dims: a tuple of the size of the 2D grid
+    """
+    patches_h, patches_w = max_dims
+    # all the windows are of the same shape
+    min_h = window_size//2
+    max_h = patches_h - min_h + 1
+
+    min_w = window_size//2
+    max_w = patches_w - min_w + 1
+    
+    # --------------------------------------------------------------------------
+    # generate different permutations
+    # --------------------------------------------------------------------------
+    # We generate the means  in such way to make sure the means do not duplicate
+    # for a single image
+    # --------------------------------------------------------------------------
+    p_h = (torch.randperm(max_h-min_h)+min_h).expand((N,-1))
+    p_w = (torch.randperm(max_w-min_w)+min_w).expand((N,-1))
+
+    noise_h = torch.rand(p_h.shape)
+    noise_w = torch.rand(p_w.shape)
+
+    ids_h_shuffle = torch.argsort(noise_h, dim=1)
+    ids_w_shuffle = torch.argsort(noise_w, dim=1)
+
+    m_h = torch.gather(p_h, dim=1, index=ids_h_shuffle)[:,:n_gaussian]
+    m_w = torch.gather(p_w, dim=1, index=ids_w_shuffle)[:,:n_gaussian]
+
+    # ==========================================================================
+    # make sure the shapes are correct
+    # ==========================================================================
+    assert m_h.shape == (N,n_gaussian)
+    assert m_w.shape == (N,n_gaussian)
+
+    return m_h, m_w
+
+# ==============================================================================
+# Here, we will generate multiple  independent gaussian distributions  and merge
+# them to have a mixture of gaussians
+def mixture_gaussians_1D(N, n_gaussian, window_sizes, patches_h, patches_w, 
+                         enable_out=True, mean_mask = True):
+    # Randomly set the mean
+    if len(window_sizes.unique()) == 1:
+        # ======================================================================
+        # Make sure the window of the gaussian fits into the grid
+        assert window_sizes[0] < patches_h and window_sizes[0] < patches_w
+        m_h , m_w = mean_generation(window_sizes[0].item(), N, n_gaussian, 
+                                    (patches_h,patches_w))
+
+
+        
+        # m_h = torch.randint(min_h,max_h,(N, n_gaussian))
+        # m_w = torch.randint(min_w,max_w,(N, n_gaussian,))
+        # m_h = (torch.randperm(max_h-min_h)+min_h)[:n_gaussian]
+        # m_w = (torch.randperm(max_w-min_w)+min_w)[:n_gaussian]
+    else:
+        pass
+        # m_h = torch.zeros(n_gaussian,dtype=int)
+        # m_w = torch.zeros(n_gaussian,dtype=int)
+        # for indx in range(len(window_sizes)):
+        #     min_h = window_sizes[indx].item()//2
+        #     max_h = patches_h - min_h + 1
+
+        #     min_w = min_h
+        #     max_w = patches_w - min_w + 1
+        #     m_h[indx] = torch.randint(min_h,max_h,(1,))
+        #     m_w[indx] = torch.randint(min_w,max_w,(1,))
+    # ==========================================================================
+    # Now generate the gaussians
+    p_mask = torch.zeros(N, patches_h*patches_w)
+    means_mask = None
+    if mean_mask:
+        means_mask = torch.zeros(p_mask.shape)
+    for gaussian_indx in range(len(window_sizes)):
+        p,means = generate_gaussian_2D_1D(patches_h, patches_w, 
+                                    m_h[:,gaussian_indx], 
+                                    m_w[:,gaussian_indx],
+                                    window_sizes[gaussian_indx].item(),
+                                    enable_out, mean_mask)
+        p_mask += p
+        if mean_mask:
+            means_mask += means
+    if mean_mask:
+        p_mask = (1-means_mask)*p_mask
+        # We have N*n_gaussian number of gaussians
+        assert means_mask.nonzero().shape[0]==N*n_gaussian
+        # Make sure that all the means have 0 probability
+        assert (((1-means_mask)*p_mask)==p_mask).prod().item() == 1
+    # Normalize your probability distribution
+    p_mask = p_mask / p_mask.sum(dim=1).reshape(-1,1)
+    return p_mask, means_mask
+
+
+# ==============================================================================
+# Sample excution
+# ==============================================================================
+# N = 16
+# new_h = 14
+# new_w = 14
+# depth = 786
+# rand_data = torch.rand(N, new_h, new_w, depth)
+# rand_data.shape
+
+# n_gaussian = 6
+# window_sizes = torch.ones(n_gaussian, dtype=torch.uint8)*9
+# p_mask, means_mask = mixture_gaussians_1D(N,n_gaussian, window_sizes, new_h,
+#                                           new_w)
+# means_idx = means_mask.nonzero()[:,1].reshape(N,-1)
+# num_samples = 18
+# idx = sample_indices_1D(p_mask, num_samples)
+# sample_mask = generate_binary_mask_1D(p_mask.shape, idx)
+# assert (sample_mask*means_mask).sum() == 0
+# ------------------------------------------------------------------------------
+# description
+# ------------------------------------------------------------------------------
+# means_mask: binary 2D mask having 1 for the selected patches for anchor tokens
+# sample_mask: binary 2D mask having 1 for sampled patches according to the gau-
+#           ssian [for reconstruction]
+# idx: the indices of the sampled patches
+# means_idx: the indices of the means; they are sorted according to position
+# ==============================================================================
